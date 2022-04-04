90921967527a7db04534683a5f62cc2d83f6721e
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-04-05 00:34:25 +0600

    custom hook and reviews component done

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import useReviews from '../../Hook/useReviews';
+import Card from '../Card/Card';
 import './Reviews.css'
 
 const Reviews = () => {
+
+    const [reviews, setReviews] = useReviews()
+    //console.log(reviews)
+
     return (
-        <div>
-            <h1>review div</h1>
+        <div className='grid lg:grid-cols-3 sm:grid-cols-1 gap-8 p-14 bg-gray-200'>
+            {
+                reviews.map(review => <Card
+                    key={review.id}
+                    review={review}
+                ></Card>)
+            }
         </div>
     );
 };
