709b1a3a7d2a0875628845eb7ddb85db02f0bd17
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-04-04 12:51:19 +0600

    fix render dom warning and add react router

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,9 +3,20 @@ import './Navbar.css'
 
 const Navbar = () => {
     return (
-        <div>
-            <h1>navbar div</h1>
-        </div>
+        <header className="text-gray-600 ">
+
+
+            <nav className="md:ml-auto  md:mr-auto flex flex-wrap items-center justify-center text-2xl mt-6">
+
+                <a className="mr-5  ">First Link</a>
+                <a className="mr-5 ">Second Link</a>
+                <a className="mr-5 ">Third Link</a>
+                <a className="mr-5 ">Fourth Link</a>
+
+            </nav>
+
+
+        </header>
     );
 };
 
