756fb9f411a93273fc77fc2f1d03f008d59e67aa
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-04-04 12:39:43 +0600

    all component creat and add

---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './About.css'
+
+const About = () => {
+    return (
+        <div>
+            <h1>about div</h1>
+        </div>
+    );
+};
+
+export default About;
