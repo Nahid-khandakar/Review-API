03dc31c07055d4fcd1c85756adc700d54f99e305
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-04-05 12:03:16 +0600

    about section updated

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,8 +3,14 @@ import './About.css'
 
 const About = () => {
     return (
-        <div>
-            <h1>about div</h1>
+        <div className=''>
+            <h1 className=" text-2xl lg:text-3xl font-medium text-red-500 text-center p-10 mt-10" >
+                “Logic will get you from A to Z, imagination will get you everywhere.”
+                <br />
+
+            </h1>
+            <h2 className=' text-2xl lg:text-3xl font-medium text-amber-400 text-center pl-14'>― Albert Einstein</h2>
+
         </div>
     );
 };
--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -3,9 +3,20 @@ import './Blogs.css'
 
 const Blogs = () => {
     return (
+
         <div>
-            <h1>all blogs</h1>
+            {/* Question 1 */}
+            <div>
+                <h1>What is CONTEXT API</h1>
+                <h3>
+                    The Context API can be used to share data with multiple components, without having to pass data through props manually.To start with the Context API, the first thing  is create a context using the createContext function from React. The createContext function accepts an initial value, but this initial value is not required.
+
+                    After creating your context, that context now has two React components that are going to be used: Provider and useContext hook.
+                    The Provider component is going to be used to wrap the components that are going to have access to our context. The useContext hook receives a prop called value, which can be accessed from all the components that are wrapped inside Provider, and it will be responsible to grant access to the context data .The useContext hook receives a single argument, which is the context that we want to access .
+                </h3>
+            </div>
         </div>
+
     );
 };
 
