4ac8f5fd0a36cb1e240db7037ddcd28bf32b4953
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-04-05 15:13:49 +0600

    navlink added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Navbar.css'
 
 const Navbar = () => {
+
     return (
-        <header className="text-gray-600 ">
+
+        <nav className="sm:ml-auto  sm:mr-auto flex flex-wrap items-center justify-center text-xl mt-8 mb-8">
 
 
-            <nav className="md:ml-auto  md:mr-auto flex flex-wrap items-center justify-center text-xl mt-10 mb-2">
-
-                <Link to='home' className='ml-4 mr-9' >HOME</Link>
-                <Link to='review' className='mr-9'> REVIEW</Link>
-                <Link to='dashboard' className='mr-9' > DASHBOARD</Link>
-                <Link to='blog' className='mr-9' >BLOG</Link>
-                <Link to='about' className='mr-9'>ABOUT</Link>
-
-            </nav>
+            <NavLink to='home' className={({ isActive }) => (isActive ? "active-link ml-8 mr-10" : "link ml-8 mr-10")}>
+                HOME
+            </NavLink>
 
 
-        </header>
+            <NavLink to='review' className={({ isActive }) => (isActive ? "active-link mr-10" : "link mr-10")}>
+                REVIEW
+            </NavLink>
+
+            <NavLink to='dashboard' className={({ isActive }) => (isActive ? "active-link mr-10 " : "link mr-10 ")} >
+                DASHBOARD
+            </NavLink>
+
+            <NavLink to='blog' className={({ isActive }) => (isActive ? "active-link mr-10" : "link mr-10")} >
+                BLOG
+            </NavLink>
+
+            <NavLink to='about' className={({ isActive }) => (isActive ? "active-link mr-10" : "link mr-10")}>
+                ABOUT
+            </NavLink>
+
+        </nav>
+
     );
 };
 
